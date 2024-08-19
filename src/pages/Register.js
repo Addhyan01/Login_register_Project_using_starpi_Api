8a362919e7e02858d7ca4b13f06d22430fe806aa
Addhyan01 <addhyankumar@gmail.com>
2024-08-20 01:48:32 +0530

    Api is working proper and data is registerd in strapi but this is hard code data

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,33 @@
 import React from 'react'
 
 export default function Register() {
+
+    const sendData = () =>{
+
+
+        let data= {
+            username: "Sonali",
+            email: "sonali@gmail.com",
+            password: "Sonali@123"
+        }
+
+        fetch(`http://localhost:1337/api/auth/local/register`, {
+            method:"post",
+            headers:{
+                 'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+        .then((res)=>{res.json()})
+        .then((data)=>{ console.log(data)})
+        .catch(()=>{})
+
+    }
+
+
+
   return (
     
     <>
@@ -23,7 +50,7 @@ export default function Register() {
             <input type="password" className="form-control" id="password" name='password' />
         </div>
 
-        <button type="button" className="btn btn-primary">Submit</button>
+        <button type="button" onClick={sendData} className="btn btn-primary">Submit</button>
     </form>
 
 
